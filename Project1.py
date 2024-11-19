8d0a3fde72e42354d03cb3d5f135b5367cd999c1
Ishrat Parvin Shaik <148193966+Ishratparvin@users.noreply.github.com>
2024-11-19 08:03:35 +0530

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -1,37 +1,58 @@
-#importing libraries
-from facial_emotion_recognition import EmotionRecognition
-import cv2
+import cv2, numpy, os
+haar_file = 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
+datasets = 'datasets'
+print('Training...')
+(images, labels, names, id) = ([], [], {}, 0)
 
-#Capturing Video
-er = EmotionRecognition(device='cpu')
-cam = cv2.VideoCapture(0)
+for (subdirs, dirs, files) in os.walk(datasets):
+    for subdir in dirs:
+        names[id] = subdir
+        subjectpath = os.path.join(datasets, subdir)
+        for filename in os.listdir(subjectpath):
+            path = subjectpath + '/' + filename
+            label = id
+            images.append(cv2.imread(path, 0))
+            labels.append(int(label))
+        id +=1
+
+(images, labels) = [numpy.array(lis) for lis in [images, labels]]
+print(images, labels)                   
+(width, height) = (130, 100)
+model = cv2.face.LBPHFaceRecognizer_create()
+#model =  cv2.face.FisherFaceRecognizer_create()
+
+model.train(images, labels)
+
+webcam = cv2.VideoCapture(0)
+cnt=0
 
 while True:
-    #return 2 values success and frame
-    success, frame = cam.read()
-    result = er.recognise_emotion(frame,return_type='GBR')
+    (_, im) = webcam.read()
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,255,0),2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
 
-    #result contains 2 values
-    if result is not None and len(result) == 2:
-        faces, emotions = result
-
-        #zip() function returns a zip object, which is an iterator of tuples where the
-        #first item in each passed iterator is paired together, and then the second item
-        #in each passed iterator are paired together etc.
-        for (x, y, w, h), emotion in zip(faces, emotions):
-            # Draw bounding box
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            # Add text with the recognized emotion
-            cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-    cv2.imshow('frame', frame)
-    key = cv2.waitKey(1)
-    #ord returns ascii value of the character
-    if key == ord("q"):
+        prediction = model.predict(face_resize)
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        if prediction[1]<800:
+            cv2.putText(im,'%s - %.0f' % (names[prediction[0]],prediction[1]),(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,2,(0, 0, 255))
+            print (names[prediction[0]])
+            cnt=0
+        else:
+            cnt+=1
+            cv2.putText(im,'Unknown',(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+            if(cnt>100):
+                print("Unknown Person")
+                cv2.imwrite("unKnown.jpg",im)
+                cnt=0
+    cv2.imshow('FaceRecognition', im)
+    key = cv2.waitKey(10)
+    if key == ord('q'):
         break
 
-#Releasing the camera
-cam.release()
-#Destroying all windows
+webcam.release()
 cv2.destroyAllWindows()
